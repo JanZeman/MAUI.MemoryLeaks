7eca493906d9adde60a4d1b44d5203e2e384a89c
Jan Zeman <jan.zeman@live.com>
2023-09-11 22:54:08 +0200

    // Workaround for the Warning: 'ValueSample' property not found on 'MAUI.MemoryLeaks.ViewModel.Case01ViewModel', target property: 'Microsoft.Maui.Controls.Label.Text'

--- MAUI.MemoryLeaks/Controls/ItemCard.xaml.cs
+++ MAUI.MemoryLeaks/Controls/ItemCard.xaml.cs
@@ -1,3 +1,6 @@
+using System.Diagnostics;
+using MAUI.MemoryLeaks.Model;
+
 namespace MAUI.MemoryLeaks.Controls;
 
 public partial class ItemCard
@@ -6,4 +9,18 @@ public partial class ItemCard
 	{
 		InitializeComponent();
 	}
+
+    protected override void OnBindingContextChanged()
+    {
+        // Workaround for the Warning: 'ValueSample' property not found on 'MAUI.MemoryLeaks.ViewModel.Case01ViewModel', target property: 'Microsoft.Maui.Controls.Label.Text'
+        if (BindingContext is not ItemSample)
+            return;
+
+        base.OnBindingContextChanged();
+
+#if DEBUG
+        // Log or debug the change
+        Debug.WriteLine($"ItemCard BindingContext is: {BindingContext?.GetType().Name ?? "null"}");
+#endif
+    }
 }
