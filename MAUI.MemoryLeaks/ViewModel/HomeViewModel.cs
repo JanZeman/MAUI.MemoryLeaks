a58c2b9d6a48c69a5801906ac52dd5b19c98cac1
Jan Zeman <jan.zeman@live.com>
2023-11-27 15:17:39 +0100

    .NET 8.0.1

--- MAUI.MemoryLeaks/ViewModel/HomeViewModel.cs
+++ MAUI.MemoryLeaks/ViewModel/HomeViewModel.cs
@@ -10,9 +10,9 @@ public partial class HomeViewModel : BaseViewModel
     public HomeViewModel()
     {
         PageName = "Leaks";
-        Motivation = "It's 2023, and we are still encountering memory leaks in MAUI, both in released and upcoming versions. This project aims to identify some of them.";
+        Motivation = "It's 2023, and we are still encountering memory leaks in MAUI. This project targets one of them and will try to target more if such appear.";
         Description =
-            "It can be reproduced on Windows. Recently tested in September 2023 (.NET 8.0.100-preview.7)." + Environment.NewLine + Environment.NewLine +
+            "It can be reproduced on Windows. Recently tested in November 2023 (.NET 8.0.1)." + Environment.NewLine + Environment.NewLine +
             "The expectation is that after a large number of items are created and then cleared from the given collection, the consumed memory should drop to the original level within a minute or two of starting the experiment. This should happen without having to navigate back to the home screen." + Environment.NewLine + Environment.NewLine +
             " - Start the Windows app" + Environment.NewLine + 
             " - Click the 'ObservableCollection' button and follow up on the page opened." + Environment.NewLine +
