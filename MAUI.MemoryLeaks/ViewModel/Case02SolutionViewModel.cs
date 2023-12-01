eadbf794dde83356a4948182ded035d971a2d177
Jan Zeman <jan.zeman@live.com>
2023-12-01 23:15:12 +0100

    Minor

--- MAUI.MemoryLeaks/ViewModel/Case02SolutionViewModel.cs
+++ MAUI.MemoryLeaks/ViewModel/Case02SolutionViewModel.cs
@@ -13,7 +13,7 @@ public class Case02SolutionViewModel : ItemsViewModel
             "This can be considered as a showstopper for any application showing non-minimalistic number of items. I haven't found any solution or workaround yet. ";
 
         AddedItemsCount = 1000;
-        RecommendationClearItems = "The UI will fully load after more than 30 seconds. For only 1.000 items! Try scrolling on the CollectionView below, wait for it, probably it is still loading...";
+        RecommendationClearItems = "The UI will fully load after more than 30 seconds. For only 1.000 items! Try scrolling on the CollectionView below, wait for it, probably it is still loading and consuming the memory...";
         RecommendationObserve = "Wait for few minutes and observe the memory behavior.";
     }
 }
