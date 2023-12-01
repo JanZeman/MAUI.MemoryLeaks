e1677c5bcfa724c636d9fd829d3771abcd6c277c
Jan Zeman <jan.zeman@live.com>
2023-12-01 22:54:47 +0100

    20-40

--- MAUI.MemoryLeaks/ViewModel/Case01ProblemViewModel.cs
+++ MAUI.MemoryLeaks/ViewModel/Case01ProblemViewModel.cs
@@ -7,7 +7,7 @@ public class Case01ProblemViewModel : ItemsViewModel
         PageName = "Case 1: Problem";
         Recommendation = RecommendationAddItems;
         Description =
-            "Even after minutes of waiting you will find the memory usage approx. 20-30 MB higher then before. " +
+            "Even after long minutes of waiting you will find the memory usage approx. 20-40 MB higher then before. " +
             "It will be freed up first after you leave this page and navigate one level up." +
             "That must probably mean that the memory is freed first after the CollectionView control is completely destroyed; not earlier when all its items got destroyed.";
 
--- MAUI.MemoryLeaks/ViewModel/Case01SolutionViewModel.cs
+++ MAUI.MemoryLeaks/ViewModel/Case01SolutionViewModel.cs
@@ -7,7 +7,7 @@ public class Case01SolutionViewModel : ItemsViewModel
         PageName = "Case 1: Solution / Workaround";
         Recommendation = RecommendationAddItems;
         Description =
-            "Compared to the first flow the memory will be freed up without having to leave this page. Just wait long enough, i.e. even minutes." +
+            "Compared to the first flow the memory will be freed up without having to leave this page. Just wait long enough, i.e. few minutes." +
             $"{Environment.NewLine}{Environment.NewLine}" +
             "It is because ObservableCollection is re-created upon each clear. See the corresponding code. That is however just a workaround IMHO.";
     }
